d4ee620e29e6ec624aa63a5e32f62f4d0d0661fd
Tangerine Cat <kaineer@gmail.com>
2011-02-04 16:46:03 +0500

    Input group..

--- lib/ream/form/input_group.rb
+++ lib/ream/form/input_group.rb
@@ -5,6 +5,29 @@ module Ream
     class InputGroup < Input
       def initialize( name = nil, type = :select, values = [], opts = {} )
         super( name, type, values, opts )
+        raise "Value for #{self.class.name} should be array" unless values.is_a?( Array )
+      end
+
+      def render
+        render_group do
+          render_content
+        end
+      end
+
+      def render_group( &block )
+        if @form && @form.render_style
+          @form.render_style.render_group( self, &block )
+        else
+          render_preamble +
+            block.call +
+            render_postamble
+        end
+      end
+
+      def render_content
+        if @form && @form.render_style
+          @form.render_style.render_group_items( self, &block )
+        end
       end
     end
   end
