605249d76f87361fcbe030163923253d9da48bda
Tangerine Cat <kaineer@gmail.com>
2011-02-08 15:53:57 +0500

    Removed include Html - do not need it there

--- lib/ream/form/checkbox_group.rb
+++ lib/ream/form/checkbox_group.rb
@@ -2,8 +2,6 @@ require 'ream/form/input_group'
 
 module Ream::Form
   class CheckboxGroup < InputGroup
-    include Ream::Form::Html
-
     def initialize( name = nil, values = [], opts = {} )
       super( name, :checkbox_group, values, opts )
     end
